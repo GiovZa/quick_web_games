56877ca77a4e269a9873eecd7e20ec0c68a4ce59
yanlinda11 <128328586+yanlinda11@users.noreply.github.com>
2024-05-08 00:31:43 -0500

    Update game navigation in Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import '../App.css';
 import Header from "../components/Header/header";
 import Carouselpage from '../components/carousel/carousel';
 import Search from '../components/Search/SearchContainer';
+import "../components/Games/UltimateTicTacToe.scss";
 
 export default function Homepage() {
     return (
@@ -10,10 +11,13 @@ export default function Homepage() {
             <Search/>
             <Carouselpage />
             <div>
-            	<a href='/UltimateTicTacToe'>Ultimate Tic-Tac-Toe</a>
-            </div>
-            <div>
-            	<a href='/MineSweeper'>MineSweeper</a>
+                  <a href='/MineSweeper'> 
+                    <button className="reset-button" style={{backgroundColor: "#30208c"}}>MineSweeper</button>
+                  </a>
+                    <button className="reset-button-space">{' '}</button>
+                  <a href='/UltimateTicTacToe'>
+                    <button className="reset-button" style={{backgroundColor: "#30208c"}}>Ultimate Tic Tac Toe</button>
+                  </a>
             </div>
             
       </div>
